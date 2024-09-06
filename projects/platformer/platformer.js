9f2e9b9bfd2c2a80d12f80bfbff033725bc4ce37
DanielNguyen0804 <daniel.nguyen2@sciacademy.org>
2024-09-06 15:09:17 +0000

    <message about commit>

--- projects/platformer/platformer.js
+++ projects/platformer/platformer.js
@@ -39,28 +39,46 @@ $(function () {
     // You must decide the x position, y position, width, and height of the platforms
     // example usage: createPlatform(x,y,width,height)
 createPlatform(45,187, 153, 8)
-createPlatform(276, 278, 153, 8)
-createPlatform(27, 351, 153, 8)
-createPlatform(550, 228, 153, 8)
+createPlatform(276, 320, 170, 8)
+createPlatform(27, 420, 153, 8)
+createPlatform(550, 235, 153, 8)
+createPlatform(740, 340, 155, 8)
+createPlatform(940, 430, 155, 8)
+createPlatform(775, 500, 170, 8)
+createPlatform(600, 450, 155, 8)
+createPlatform(1000, 630, 155, 8)
+
+createPlatform(550, 228, 10, 120)
+createPlatform(1200, 500, 10, 1000)
+
     
     // TODO 2
     // Create collectables
     // You must decide on the collectable type, the x position, the y position, the gravity, and the bounce strength
     // Your collectable choices are 'database' 'diamond' 'grace' 'kennedi' 'max' and 'steve'; more can be added if you wish
     // example usage: createCollectable(type, x, y, gravity, bounce)
-
-
-
+createCollectable("database", 335, 280)
+createCollectable("database", 80, 380)
+createCollectable("database", 600, 195)
+createCollectable("database", 795, 300)
+createCollectable("database", 995, 390)
+createCollectable("database", 835, 460)
+createCollectable("database", 660, 410)
+createCollectable("database", 400, 700)
+createCollectable("database", 1285, 700)
+createCollectable("database", 1185, 464)
     
     // TODO 3
     // Create cannons
     // You must decide the wall you want the cannon on, the position on the wall, and the time between shots in milliseconds
     // Your wall choices are: 'top' 'left' 'right' and 'bottom'
     // example usage: createCannon(side, position, delay, width, height)
-
-
-
-    
+createCannon("left", 75, 500)
+createCannon("top", 420, 700)
+createCannon("top", 700,950)
+createCannon("right", 450, 900)
+createCannon("left", 630, 1200)
+createCannon("top", 600, 1200)
     /////////////////////////////////////////////////
     //////////ONLY CHANGE ABOVE THIS POINT///////////
     /////////////////////////////////////////////////
